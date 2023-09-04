a03b76a33a9ad178cf63208b4b604bb9eb6114c1
Vadim Glumakov <glumakoffv@yandex.ru>
2023-09-04 18:16:34 +0300

    14исправил

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,14 @@ const { auth } = require("../middleware/auth");
 const router = express.Router();
 const userRouter = require("./users");
 const cardRouter = require("./cards");
+const NotFoundError = require("../errors/NotFoundError");
 
 router.post("/signin", validationLogin, login);
 router.post("/signup", validationCreateUser, createUser);
 router.use(auth);
 router.use(userRouter);
 router.use(cardRouter);
-router.use((req, res) => {
-    res.status(404).send({ message: "Такой страницы не существует" });
+router.use((req, res, next) => {
+    next(new NotFoundError("Такой страницы не существует"));
 });
 module.exports = router;
